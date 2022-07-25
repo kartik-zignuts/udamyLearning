30a8358a7c714da5801613990505b6733d471d83
kartik-zignuts <kartikp@zignuts.com>
2022-07-25 18:51:14 +0530

    frontend completed

--- lib/widget/manage_producdt_item.dart
+++ lib/widget/manage_producdt_item.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:untitled/provider/products_provider.dart';
+import 'package:untitled/screens/edit_product_screen.dart';
 
 class ManageProductItem extends StatelessWidget {
+  final String id;
   final String titlel;
   final String imageUrl;
 
   const ManageProductItem(
-      {Key? key, required this.titlel, required this.imageUrl})
+      {Key? key,
+      required this.id,
+      required this.titlel,
+      required this.imageUrl})
       : super(key: key);
 
   @override
@@ -17,13 +24,39 @@ class ManageProductItem extends StatelessWidget {
         child: Row(
           children: [
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.pushNamed(context, EditProductScreen.routeName,
+                      arguments: id);
+                },
                 icon: Icon(
                   Icons.edit,
                   color: Theme.of(context).colorScheme.primary,
                 )),
             IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                            title: const Text('Are you sure?'),
+                            content: const Text(
+                                'Do you want to remove this product?'),
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Provider.of<ProductsProvider>(context,
+                                            listen: false)
+                                        .deleteProduct(context, id);
+                                    Navigator.of(ctx).pop();
+                                  },
+                                  child: const Text('Yes')),
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(ctx).pop();
+                                  },
+                                  child: const Text('No'))
+                            ],
+                          ));
+                },
                 icon: Icon(Icons.delete, color: Theme.of(context).errorColor))
           ],
         ),
